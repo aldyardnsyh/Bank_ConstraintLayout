351229612cff2ba6d65922b0c41598c466639167
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-13 23:02:46 +0700

    First commit

---
+++ app/src/main/java/com/example/homescreen_constraintlayout/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.homescreen_constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homescreen_constraintlayout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(R.layout.activity_main)
+    }
+}
